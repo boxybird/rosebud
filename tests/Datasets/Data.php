c512e30b7f058b69c00a0fcec3ef9b4b93132738
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-12 21:35:09 -0700

    Add new dataset for movie details in tests

--- tests/Datasets/Data.php
+++ tests/Datasets/Data.php
@@ -4,6 +4,10 @@ dataset('movies', [
     [getResponse('fight-club.json')],
 ]);
 
+dataset('movie', [
+    [getResponse('fight-club-details.json')],
+]);
+
 dataset('tv shows', [
     [getResponse('breaking-bad.json')],
 ]);
