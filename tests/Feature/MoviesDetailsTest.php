8adaa0749d2002c30eab9f4be9677318a5957c99
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-12 20:49:12 -0700

    Update test to use 'Dawn of the Dead' movie details

--- tests/Feature/MoviesDetailsTest.php
+++ tests/Feature/MoviesDetailsTest.php
@@ -6,7 +6,7 @@ use Rosebud\Tmdb;
 it('can get movie details', function () {
     $api_key = getTestingEnv('TMDB_API_KEY');
 
-    $movie = (new Tmdb(api_key: $api_key))->movieDetails(550); // Fight Club
+    $movie = (new Tmdb(api_key: $api_key))->movieDetails(924); // Dawn of the Dead (2004)
 
     expect($movie)->toBeInstanceOf(MovieDetailData::class);
 });
