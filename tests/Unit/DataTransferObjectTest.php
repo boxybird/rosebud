b470d15ec75dedc01b1c78493d90303aabbd0560
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-12 20:44:46 -0700

    Add test for creating TV data transfer objects

--- tests/Unit/DataTransferObjectTest.php
+++ tests/Unit/DataTransferObjectTest.php
@@ -1,9 +1,16 @@
 <?php
 
 use Rosebud\DataTransferObjects\MovieData;
+use Rosebud\DataTransferObjects\TvData;
 
-it('can create a data movie transfer object', function ($movie) {
+it('can create a movie data transfer object', function (array $movie) {
     $movie = MovieData::fromArray($movie);
 
     expect($movie)->toBeInstanceOf(MovieData::class);
 })->with('movies');
+
+it('can create a tv data transfer object', function (array $tv_show) {
+    $tv_show = TvData::fromArray($tv_show);
+
+    expect($tv_show)->toBeInstanceOf(TvData::class);
+})->with('tv shows');
