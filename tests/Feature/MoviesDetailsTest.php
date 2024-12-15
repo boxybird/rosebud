3dfb7f9ab194185abae5f8b0a9247192a0ae9e7d
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-15 11:38:15 -0700

    Refactor test to use `Movie` class instead of `Tmdb`.

--- tests/Feature/MoviesDetailsTest.php
+++ tests/Feature/MoviesDetailsTest.php
@@ -1,12 +1,12 @@
 <?php
 
 use Rosebud\DataTransferObjects\MovieDetailData;
-use Rosebud\Tmdb;
+use Rosebud\Movie;
 
 it('can get movie details', function () {
     $api_key = getTestingEnv('TMDB_API_KEY');
 
-    $movie = (new Tmdb(api_key: $api_key))->movieDetails(924); // Dawn of the Dead (2004)
+    $movie = (new Movie(api_key: $api_key))->details(924); // Dawn of the Dead (2004)
 
     expect($movie)->toBeInstanceOf(MovieDetailData::class);
 });
