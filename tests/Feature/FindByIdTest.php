339afec63374083bffd0b0db1c5872c7a3338666
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-15 11:38:04 -0700

    Refactor `findByImdbId` tests to use specific classes

--- tests/Feature/FindByIdTest.php
+++ tests/Feature/FindByIdTest.php
@@ -2,38 +2,60 @@
 
 use Rosebud\DataTransferObjects\MovieData;
 use Rosebud\DataTransferObjects\PersonData;
-use Rosebud\DataTransferObjects\TvData;
 use Rosebud\DataTransferObjects\TvEpisodeData;
-use Rosebud\Tmdb;
+use Rosebud\DataTransferObjects\TvShowData;
+use Rosebud\Enums\ExternalSourcesEnum;
+use Rosebud\Movie;
+use Rosebud\Person;
+use Rosebud\TvEpisode;
+use Rosebud\TvShow;
 
 it('finds movie by imdb id', function () {
     $api_key = getTestingEnv('TMDB_API_KEY');
 
-    $movie = (new Tmdb(api_key: $api_key))->findByImdbId('tt0137523'); // Fight Club
+    $movie = (new Movie(api_key: $api_key))->find('tt0137523'); // Fight Club
 
-    expect($movie)->toBeInstanceOf(MovieData::class);
+    expect($movie)
+        ->toBeInstanceOf(MovieData::class)
+        ->and($movie->title)->toBe('Fight Club');
 });
 
 it('finds person by imdb id', function () {
     $api_key = getTestingEnv('TMDB_API_KEY');
 
-    $person = (new Tmdb(api_key: $api_key))->findByImdbId('nm0000093'); // Brad Pitt
+    $person = (new Person(api_key: $api_key))->find('nm0000093'); // Brad Pitt
 
-    expect($person)->toBeInstanceOf(PersonData::class);
+    expect($person)
+        ->toBeInstanceOf(PersonData::class)
+        ->and($person->name)->toBe('Brad Pitt');
 });
 
 it('finds tv show by imdb id', function () {
     $api_key = getTestingEnv('TMDB_API_KEY');
 
-    $tv = (new Tmdb(api_key: $api_key))->findByImdbId('tt0903747'); // Breaking Bad
+    $tv = (new TvShow(api_key: $api_key))->find('tt0903747'); // Breaking Bad
 
-    expect($tv)->toBeInstanceOf(TvData::class);
+    expect($tv)
+        ->toBeInstanceOf(TvShowData::class)
+        ->and($tv->name)->toBe('Breaking Bad');
 });
 
 it('finds tv episode by imdb id', function () {
     $api_key = getTestingEnv('TMDB_API_KEY');
 
-    $tv = (new Tmdb(api_key: $api_key))->findByImdbId('tt0697679'); // Seinfeld - The Contest Episode
+    $tv = (new TvEpisode(api_key: $api_key))->find('tt0697679'); // Seinfeld - The Contest Episode
 
-    expect($tv)->toBeInstanceOf(TvEpisodeData::class);
+    expect($tv)
+        ->toBeInstanceOf(TvEpisodeData::class)
+        ->and($tv->name)->toBe('The Contest');
+});
+
+it('finds tv show by tvdb id', function () {
+    $api_key = getTestingEnv('TMDB_API_KEY');
+
+    $tv_show = (new TvShow(api_key: $api_key))->find(81189, ExternalSourcesEnum::TVDB); // Breaking Bad
+
+    expect($tv_show)
+        ->toBeInstanceOf(TvShowData::class)
+        ->and($tv_show->name)->toBe('Breaking Bad');
 });
