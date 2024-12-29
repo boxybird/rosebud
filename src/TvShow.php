297a6f549cdc2476d19e76ec710549f054e19e1d
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-29 08:09:15 -0700

    Add method to fetch TV show details with enriched data

--- src/TvShow.php
+++ src/TvShow.php
@@ -3,6 +3,7 @@
 namespace Rosebud;
 
 use Rosebud\DataTransferObjects\TvSeries\TvShowData;
+use Rosebud\DataTransferObjects\TvSeries\TvShowDetailsData;
 use Rosebud\Enums\ExternalSourcesEnum;
 
 class TvShow extends Tmdb
@@ -12,5 +13,14 @@ class TvShow extends Tmdb
         $data = $this->findByID($id, $external_source, $times, $sleep);
 
         return $data ?? null;
+    }
+
+    public function details(int $id, int $times = 2, int $sleep = 2000): TvShowDetailsData
+    {
+        $data = $this->get($this->base_url.'/movie/'.$id, [
+            'append_to_response' => 'alternative_titles,credits,external_ids,images,keywords,latest,recommendations,release_dates,reviews,similar,releases,translations,videos,providers',
+        ], $times, $sleep);
+
+        return TvShowDetailsData::fromArray($data);
     }
 }
