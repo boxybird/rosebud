51c8a8592452381621c5bd5d2fee48cb0564d087
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-29 08:09:02 -0700

    Add test for fetching TV show details

--- tests/Unit/TvShowTest.php
+++ tests/Unit/TvShowTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Rosebud\DataTransferObjects\TvSeries\TvShowData;
+use Rosebud\DataTransferObjects\TvSeries\TvShowDetailsData;
 use Rosebud\Enums\ExternalSourcesEnum;
 use Rosebud\TvShow;
 
@@ -14,11 +15,20 @@ it('finds tv show by imdb id', function () {
         ->and($tv->name)->toBe('Breaking Bad');
 });
 
+
+it('can get tv show details', function () {
+    $api_key = getTestingEnv('TMDB_API_KEY');
+
+    $tv_show = (new TvShow(api_key: $api_key))->details(924); // Dawn of the Dead (2004)
+
+    expect($tv_show)->toBeInstanceOf(TvShowDetailsData::class);
+});
+
 it('finds tv show by tvdb id', function () {
     $api_key = getTestingEnv('TMDB_API_KEY');
 
     $tv_show = (new TvShow(api_key: $api_key))->find('81189', ExternalSourcesEnum::TVDB); // Breaking Bad
-    
+
     expect($tv_show)
         ->toBeInstanceOf(TvShowData::class)
         ->and($tv_show->name)->toBe('Breaking Bad');
