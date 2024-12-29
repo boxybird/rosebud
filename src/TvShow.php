f749c56308db31a62114ea4d77f514719101f5d8
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-29 11:25:43 -0700

    Fix TvShow data validation and update API endpoint usage

--- src/TvShow.php
+++ src/TvShow.php
@@ -12,13 +12,13 @@ class TvShow extends Tmdb
     {
         $data = $this->findByID($id, $external_source, $times, $sleep);
 
-        return $data ?? null;
+        return $data instanceof TvShowData ? $data : null;
     }
 
     public function details(int $id, int $times = 2, int $sleep = 2000): TvShowDetailsData
     {
-        $data = $this->get($this->base_url.'/movie/'.$id, [
-            'append_to_response' => 'alternative_titles,credits,external_ids,images,keywords,latest,recommendations,release_dates,reviews,similar,releases,translations,videos,providers',
+        $data = $this->get($this->base_url.'/tv/'.$id, [
+            'append_to_response' => 'aggregate_credits,alternative_titles,content_ratings,credits,external_ids,images,keywords,recommendations,reviews,screened_theatrically,similar,translations,videos',
         ], $times, $sleep);
 
         return TvShowDetailsData::fromArray($data);
