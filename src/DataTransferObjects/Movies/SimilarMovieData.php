6df51ef630d9d363694566e958352cfe5dc88ef3
Andrew Rhyand <andrew.rhyand@gmail.com>
2024-12-30 10:20:26 -0700

    Make DTO properties nullable and handle missing data gracefully

--- src/DataTransferObjects/Movies/SimilarMovieData.php
+++ src/DataTransferObjects/Movies/SimilarMovieData.php
@@ -9,21 +9,21 @@ use Rosebud\DataTransferObjects\ComputedData;
 final readonly class SimilarMovieData
 {
     public function __construct(
-        public bool $adult,
+        public ?bool $adult,
         public ?string $backdrop_path,
         /** @var int[] */
-        public array $genre_ids,
+        public ?array $genre_ids,
         public int $id,
-        public string $original_language,
-        public string $original_title,
-        public string $overview,
-        public float $popularity,
+        public ?string $original_language,
+        public ?string $original_title,
+        public ?string $overview,
+        public ?float $popularity,
         public ?string $poster_path,
-        public string $release_date,
-        public string $title,
-        public bool $video,
-        public float $vote_average,
-        public int $vote_count,
+        public ?string $release_date,
+        public ?string $title,
+        public ?bool $video,
+        public ?float $vote_average,
+        public ?int $vote_count,
         public ComputedData $computed
     ) {
     }
@@ -31,20 +31,20 @@ final readonly class SimilarMovieData
     public static function fromArray(array $data): self
     {
         return new self(
-            adult: $data['adult'],
-            backdrop_path: $data['backdrop_path'],
-            genre_ids: $data['genre_ids'],
+            adult: $data['adult'] ?? null,
+            backdrop_path: $data['backdrop_path'] ?? null,
+            genre_ids: $data['genre_ids'] ?? null,
             id: $data['id'],
-            original_language: $data['original_language'],
-            original_title: $data['original_title'],
-            overview: $data['overview'],
-            popularity: $data['popularity'],
-            poster_path: $data['poster_path'],
-            release_date: $data['release_date'],
-            title: $data['title'],
-            video: $data['video'],
-            vote_average: $data['vote_average'],
-            vote_count: $data['vote_count'],
+            original_language: $data['original_language'] ?? null,
+            original_title: $data['original_title'] ?? null,
+            overview: $data['overview'] ?? null,
+            popularity: $data['popularity'] ?? null,
+            poster_path: $data['poster_path'] ?? null,
+            release_date: $data['release_date'] ?? null,
+            title: $data['title'] ?? null,
+            video: $data['video'] ?? null,
+            vote_average: $data['vote_average'] ?? null,
+            vote_count: $data['vote_count'] ?? null,
             computed: ComputedData::fromArray($data),
         );
     }
