302641c36294502046b70554b4cc307d69a1faaf
Andrew Rhyand <andrew.rhyand@gmail.com>
2025-01-04 10:04:19 -0700

    Add test for PersonDetailsData DTO functionality

--- tests/Unit/DataTransferObjectTest.php
+++ tests/Unit/DataTransferObjectTest.php
@@ -3,6 +3,7 @@
 use Rosebud\DataTransferObjects\Movies\MovieData;
 use Rosebud\DataTransferObjects\Movies\MovieDetailsData;
 use Rosebud\DataTransferObjects\People\PersonData;
+use Rosebud\DataTransferObjects\People\PersonDetailsData;
 use Rosebud\DataTransferObjects\TvEpisodes\TvEpisodeData;
 use Rosebud\DataTransferObjects\TvSeries\TvShowData;
 use Rosebud\DataTransferObjects\TvSeries\TvShowDetailsData;
@@ -48,7 +49,21 @@ it('can create a person data transfer object and then back to matching array', f
 
     expect($person_array)->toBeArray()
         ->and($person_array)->toHaveKeys($properties);
-})->with('people');
+})->with('person');
+
+it('can create a person detail data transfer object and then back to matching array', function (array $person) {
+    $person_data = PersonDetailsData::fromArray($person);
+
+    expect($person_data)->toBeInstanceOf(PersonDetailsData::class);
+
+    $reflection = new ReflectionClass($person_data);
+    $properties = array_map(fn($property) => $property->getName(), $reflection->getProperties());
+
+    $person_array = $person_data->toArray();
+
+    expect($person_array)->toBeArray()
+        ->and($person_array)->toHaveKeys($properties);
+})->with('person details');
 
 it('can create a tv show data transfer object and then back to matching array', function (array $tv_show) {
     $tv_show_data = TvShowData::fromArray($tv_show);
