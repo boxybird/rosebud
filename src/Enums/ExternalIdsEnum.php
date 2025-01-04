7f8d565a39e58cd213bab06d3d164b86577040a3
Andrew Rhyand <andrew.rhyand@gmail.com>
2025-01-04 10:02:56 -0700

    Rename and update ExternalSourcesEnum to ExternalIdsEnum

--- src/Enums/ExternalIdsEnum.php
+++ src/Enums/ExternalIdsEnum.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace Rosebud\Enums;
 
-enum ExternalSourcesEnum: string
+enum ExternalIdsEnum: string
 {
     case IMDB = 'imdb_id';
+    case WIKIDATA = 'wikidata_id';
     case FACEBOOK = 'facebook_id';
     case INSTAGRAM = 'instagram_id';
-    case TVDB = 'tvdb_id';
-    case TIKTOK = 'tiktok_id';
     case TWITTER = 'twitter_id';
-    case WIKIDATA = 'wikidata_id';
+    case FREEBASE = 'freebase_id';
+    case FREEBASE_MID = 'freebase_mid';
+    case TVDB = 'tvdb_id';
+    case TVRAGE = 'tvrage_id';
+    case TIKTOK = 'tiktok_id';
     case YOUTUBE = 'youtube_id';
 }
