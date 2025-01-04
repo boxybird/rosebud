17d32c9fef2a1cce1c7c8773e6065575ed5fb61f
Andrew Rhyand <andrew.rhyand@gmail.com>
2025-01-04 10:04:40 -0700

    Add tests for fetching person details

--- tests/Unit/PersonTest.php
+++ tests/Unit/PersonTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Rosebud\DataTransferObjects\People\PersonData;
+use Rosebud\DataTransferObjects\People\PersonDetailsData;
 use Rosebud\Person;
 
 it('finds person by imdb id', function () {
@@ -20,5 +21,25 @@ it('finds person by imdb id and return dto', function () {
 
     expect($person)
         ->toBeInstanceOf(PersonData::class)
+        ->and($person->name)->toBe('Brad Pitt');
+});
+
+it('can get person details', function () {
+    $api_key = getTestingEnv('TMDB_API_KEY');
+
+    $person = (new Person(api_key: $api_key))->details(287, raw: true); // Brad Pitt
+
+    expect($person)
+        ->toBeArray()
+        ->and($person['name'])->toBe('Brad Pitt');
+});
+
+it('can get person details and return dto', function () {
+    $api_key = getTestingEnv('TMDB_API_KEY');
+
+    $person = (new Person(api_key: $api_key))->details(287); // Brad Pitt
+
+    expect($person)
+        ->toBeInstanceOf(PersonDetailsData::class)
         ->and($person->name)->toBe('Brad Pitt');
 });
