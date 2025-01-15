c567c2c7aed73d51e2007e3a657019a186e2adb8
Andrew Rhyand <andrew.rhyand@gmail.com>
2025-01-15 08:59:49 -0700

    Add test for retrieving TV episode details

--- tests/Unit/TvEpisodeTest.php
+++ tests/Unit/TvEpisodeTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Rosebud\DataTransferObjects\TvEpisodes\TvEpisodeData;
+use Rosebud\DataTransferObjects\TvEpisodes\TvEpisodeDetailsData;
 use Rosebud\TvEpisode;
 
 it('finds tv episode by imdb id', function (): void {
@@ -21,4 +22,14 @@ it('finds tv episode by imdb id and returns dto', function (): void {
     expect($tv_episode)
         ->toBeInstanceOf(TvEpisodeData::class)
         ->and($tv_episode->name)->toBe('Ozymandias');
+});
+
+it('can get tv episode details', function (): void {
+    $api_key = getTestingEnv('TMDB_API_KEY');
+
+    $tv_episode_details = (new TvEpisode(api_key: $api_key))->details(1396, 5, 14); // Breaking Bad - Ozymandias
+
+    expect($tv_episode_details)
+        ->toBeInstanceOf(TvEpisodeDetailsData::class)
+        ->and($tv_episode_details->name)->toBe('Ozymandias');
 });
