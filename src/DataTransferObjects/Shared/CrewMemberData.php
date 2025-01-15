eee066296f572134a1ce8bae2f38ee41f8f18d66
Andrew Rhyand <andrew.rhyand@gmail.com>
2025-01-15 09:00:26 -0700

    Refactor DTOs for shared usage and update nullability

--- src/DataTransferObjects/Shared/CrewMemberData.php
+++ src/DataTransferObjects/Shared/CrewMemberData.php
@@ -9,34 +9,34 @@ use Rosebud\Enums\GendersEnum;
 final readonly class CrewMemberData
 {
     public function __construct(
-        public bool $adult,
+        public ?bool $adult,
         public ?GendersEnum $gender,
         public int $id,
         public ?string $known_for_department,
-        public string $name,
-        public string $original_name,
-        public float $popularity,
+        public ?string $name,
+        public ?string $original_name,
+        public ?float $popularity,
         public ?string $profile_path,
         public string $credit_id,
-        public string $department,
-        public string $job
+        public ?string $department,
+        public ?string $job
     ) {
     }
 
     public static function fromArray(array $data): self
     {
         return new self(
-            adult: $data['adult'],
+            adult: $data['adult'] ?? null,
             gender: $data['gender'] ?? null ? GendersEnum::fromInt((int) $data['gender']) : null,
             id: $data['id'],
             known_for_department: $data['known_for_department'] ?? null,
-            name: $data['name'],
-            original_name: $data['original_name'],
-            popularity: $data['popularity'],
+            name: $data['name'] ?? null,
+            original_name: $data['original_name'] ?? null,
+            popularity: $data['popularity'] ?? null,
             profile_path: $data['profile_path'],
             credit_id: $data['credit_id'],
-            department: $data['department'],
-            job: $data['job']
+            department: $data['department'] ?? null,
+            job: $data['job'] ?? null
         );
     }
 
