ee2156afb15c6f8bde7ae6a43990895dd58f6320
Andrew Rhyand <andrew.rhyand@gmail.com>
2025-01-15 09:00:50 -0700

    Add `details` method to TvEpisode with helper function

--- src/TvEpisode.php
+++ src/TvEpisode.php
@@ -3,6 +3,7 @@
 namespace Rosebud;
 
 use Rosebud\DataTransferObjects\TvEpisodes\TvEpisodeData;
+use Rosebud\DataTransferObjects\TvEpisodes\TvEpisodeDetailsData;
 use Rosebud\Enums\ExternalIdsEnum;
 
 class TvEpisode extends Tmdb
@@ -15,5 +16,31 @@ class TvEpisode extends Tmdb
         int $sleep = self::DEFAULT_SLEEP
     ): TvEpisodeData|array|null {
         return parent::findByID($id, $external_source, $raw, $times, $sleep);
+    }
+
+    public function details(
+        int $series_id,
+        int $season_number,
+        int $episode_number,
+        bool $raw = false,
+        int $times = self::DEFAULT_TIMES,
+        int $sleep = self::DEFAULT_SLEEP
+    ): TvEpisodeDetailsData|array {
+        $data = $this->get($this->base_url.'/tv/'.$series_id.'/season/'.$season_number.'/episode/'.$episode_number, [
+            'append_to_response' => $this->getDetailsAppendParams()
+        ], $times, $sleep);
+
+        return $raw ? $data : TvEpisodeDetailsData::fromArray($data);
+    }
+
+    protected function getDetailsAppendParams(): string
+    {
+        return implode(',', [
+            'credits',
+            'external_ids',
+            'images',
+            'translations',
+            'videos',
+        ]);
     }
 }
